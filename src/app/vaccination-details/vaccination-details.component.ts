9b72896281d767de4e0f6656fad50e053a6e940c
aaaalan <alangorgol@hotmail.com>
2021-05-10 00:13:47 +0200

    2 files modified

--- src/app/vaccination-details/vaccination-details.component.ts
+++ src/app/vaccination-details/vaccination-details.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Vaccination } from '../shared/location';
+import { LocationFactory } from '../shared/location-factory';
+import { VaccinationFactory } from '../shared/vaccination-factory';
+import { VaccinationStoreService } from '../shared/vaccination-store.service';
 
 @Component({
   selector: 'app-vaccination-details',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vaccination-details.component.css']
 })
 export class VaccinationDetailsComponent implements OnInit {
+  vaccination: Vaccination = VaccinationFactory.empty();
 
-  constructor() { }
+  @Output() showListEvent = new EventEmitter<any>();
+  constructor(
+    private vs: VaccinationStoreService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  showLocationList() {
+    this.showListEvent.emit();
   }
 
+  removeVaccination() {
+    if (confirm('Vaccination' + this.vaccination.id + ' wirklich löschen?')) {
+      this.vs
+        .remove(this.vaccination.id)
+        .subscribe(res =>
+          this.router.navigate(['../'], { relativeTo: this.route })
+        );
+    }
+  }
+
+  ngOnInit() {
+    const params = this.route.snapshot.params;
+    console.log(+params['id']);
+    this.vs.getSingle(+params['id']).subscribe(l => (this.vaccination = l));
+  }
 }
