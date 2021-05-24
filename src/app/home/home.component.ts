ad24b6805e550e8ace604e05e7a39abf96f9a714
aaaalan <alangorgol@hotmail.com>
2021-05-24 22:45:42 +0200

    2 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from '../shared/authentication-service';
 import { Location } from '../shared/location';
+import { User } from '../shared/user';
+import { UserStoreService } from '../shared/user-store.service';
 
 @Component({
   selector: 'pwr-home',
@@ -8,14 +11,32 @@ import { Location } from '../shared/location';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  loggedInUser: User;
 
-  constructor(private router: Router, private route :ActivatedRoute) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private us: UserStoreService, 
+    private authService: AuthenticationService
+  ) {}
 
-  locationSelected(location: Location){
-    this.router.navigate(['../locations', location.id], {relativeTo:this.route});
+  locationSelected(location: Location) {
+    this.router.navigate(['../locations', location.id], {
+      relativeTo: this.route
+    });
   }
+isLoggedIn() {
+   
+    return this.authService.isLoggedIn();
+  }
+
+
 
   ngOnInit() {
+        if (this.authService.isLoggedIn()) {
+      this.us
+        .getSingle(localStorage.userId)
+        .subscribe(res => (this.loggedInUser = res));
+    }
   }
-
 }
